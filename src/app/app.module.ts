40c00684c341677fbba9a2a3f1da8e3fd4de3bba
Oscar de Dios <oscar.dedios.rodriguez@gmail.com>
2017-02-17 08:48:29 +0100

    Cambios cambiados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { OtherComponent } from './other/other.component';
+import { AnotherComponent } from './other/another.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OtherComponent,
+    AnotherComponent,
+    AnotherComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/other/other.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fa-other',
+  templateUrl: './other.component.html',
+styles: [` 
+  p {
+    color:blue;
+  }`]
+})
+export class OtherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
